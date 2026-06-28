cbab74bf31da6b220bc93c3bcc5c998c8c38a4d2
serkor1 <77464572+serkor1@users.noreply.github.com>
2026-06-28 15:24:34 +0200

    :hammer: Arrays are always passed as pointers.
    * There is no reason to alias the pointer.

--- src/map/map.c
+++ src/map/map.c
@@ -6,21 +6,18 @@ void map(
     int length,
     void (*function)(int* input, int* output)
 ) {
-    // extract pointers to input/output
-    // arrays
-    int *input_pointer = input;
-    int *output_pointer = output;
 
     for (int i = 0; i < length; i++) {
          function(
-            /* input */ input_pointer,
-            /* output */ output_pointer
+            /* input */ input,
+            /* output */ output
         );
 
         // increment pointers
         // to move the location 
         // in memory
-        output_pointer++;
-        input_pointer++;
+        output++;
+        input++;
     } 
+
 }
